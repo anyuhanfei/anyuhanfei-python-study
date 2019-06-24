d927d13b5d8f9281c2c1d07e7c3186d2f919dca1
anyuhanfei <1223050251@qq.com>
2019-06-24 14:09:25 +0800

    Standard code

--- algorithm/Imooc-Algorithm-PythonEdition-master/冒泡排序_bubble_sort.py
+++ algorithm/Imooc-Algorithm-PythonEdition-master/冒泡排序_bubble_sort.py
@@ -4,7 +4,7 @@
 它重复地走访过要排序的元素列，依次比较两个相邻的元素，如果他们的顺序（如从大到小、首字母从A到Z）错误就把他们交换过来。走访元素的工作是重复地进行直到没有相邻元素需要交换，也就是说该元素已经排序完成。
 '''
 
-from .generate_random_list import GenerateRandomList
+from generate_random_list import GenerateRandomList
 
 
 def bubble_sort(disorder_list):
